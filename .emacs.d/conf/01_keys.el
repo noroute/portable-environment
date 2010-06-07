29f956c014d2d4c7d856e422c06f411a4fd1a591
Florian Thiel <flo.thiel+github@googlemail.com>
2010-06-07 17:43:55 +0200

    migration change (leaving baku)

--- .emacs.d/conf/01_keys.el
+++ .emacs.d/conf/01_keys.el
@@ -6,27 +6,6 @@
 ; annoying ctrl-z!
 (global-set-key "\C-z" nil)
 
-(defun make-open-file (filename)
-  "produce an interactive open-file command for use with set-key"
-  (lambda ()
-    (
-     (interactive)
-     (find-file filename))))
-
-; edit files (prefix: <f6>)
-(global-set-key (kbd "<f6> e") (make-open-file "~/.emacs.d/conf"))
-(global-set-key (kbd "<f6> z") (make-open-file "~/.zshrc"))
-
-; outline-mode
-(global-set-key [M-left] 'hide-body)
-(global-set-key [M-right] 'show-all)
-(global-set-key [M-up] 'outline-previous-heading)
-(global-set-key [M-down] 'outline-next-heading)
-(global-set-key [C-M-left] 'hide-sublevels)
-(global-set-key [C-M-right] 'show-children)
-(global-set-key [C-M-up] 'outline-previous-visible-heading)
-(global-set-key [C-M-down] 'outline-next-visible-heading)
-
 (defun font-inc-size ()
   (interactive)
   (set-face-attribute 'default
@@ -42,8 +21,25 @@
                       (floor (* 0.9
                                   (face-attribute 'default :height)))))
 
+(defun switch-to (buffername)
+  (lambda ()
+    (interactive)
+    (switch-to-buffer buffername)))
+
+(defun start-shell ()
+  (lambda ()
+    (interactive)
+    (ansi-term "screen" "screen")))
+
 (global-set-key (kbd "C-+") 'font-inc-size)
 (global-set-key (kbd "C--") 'font-dec-size)
+
+; views
+(global-set-key (kbd "<f1>") (switch-to "*scratch*"))
+(global-set-key (kbd "<f2>") '(lambda () ( (interactive) (ansi-term "screen" "screen"))))
+(global-set-key (kbd "<f5>") 'org-agenda)
+(global-set-key (kbd "<f6>") 'calendar)
+
 
 ;; Jump to a definition in the current file. (This is awesome.)
 (global-set-key (kbd "C-x C-i") 'ido-imenu)
