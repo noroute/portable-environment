7b406939ea1b1121a4d01795a6e2bdecf2978eda
Florian Thiel <f.thiel@tarent.de>
2010-09-25 15:27:57 +0200

    - cleanups, more autoloads, less custom libraries - added js2

--- .emacs.d/conf/02_custom-variables.el
+++ .emacs.d/conf/02_custom-variables.el
@@ -2,3 +2,5 @@
 
 (setq flyspell-default-dictionary "deutsch8")
 (setq python-python-command "python2.5")
+(setq browse-url-generic-program (executable-find "firefox")
+      browse-url-browser-function 'browse-url-generic)
