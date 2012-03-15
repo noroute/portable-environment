e6923ab5d763dff0d6664f7300d71de828935f82
Florian Thiel <f.thiel@tarent.de>
2012-03-15 14:50:58 +0100

    [Keys] Make M-x available w/o ALT, let C-w do backward-kill-word, C-xC-k kill-region

--- .emacs.d/conf/01_keys.el
+++ .emacs.d/conf/01_keys.el
@@ -3,6 +3,15 @@
 (global-set-key "\C-cg" 'goto-line)
 (global-set-key "\C-cu" 'uncomment-region)
 
+; prefer backward-kill-word!
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+; do M-x without ALT
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
 ; annoying ctrl-z!
 (global-set-key "\C-z" nil)
 
