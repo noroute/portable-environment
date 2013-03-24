f7beda5ae4342d0359cd4b81e3d3b0d9c8db5448
Florian Thiel <f.thiel@tarent.de>
2013-03-24 14:20:25 +0100

    [dired] Display and update leass obstrusive

--- .emacs.d/conf/07_cleanup.el
+++ .emacs.d/conf/07_cleanup.el
@@ -20,3 +20,13 @@
           (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                     ,(make-char 'greek-iso8859-7 107))
                     nil))))))
+
+;; Make dired less verbose
+(require 'dired-details)
+(setq-default dired-details-hidden-string "--- ")
+(dired-details-install)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
