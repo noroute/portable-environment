032a90411d2acb51b447e49c3f2a3c8aaa80f094
Florian Thiel <f.thiel@tarent.de>
2013-03-24 14:18:48 +0100

    Remove unused/prelude-provided keybindings, add movement

--- .emacs.d/conf/02_keys.el
+++ .emacs.d/conf/02_keys.el
@@ -1,8 +1,3 @@
-
-; general
-(global-set-key "\C-cg" 'goto-line)
-(global-set-key "\C-cu" 'uncomment-region)
-
 ; prefer backward-kill-word!
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
@@ -20,55 +15,57 @@
     (interactive)
     (switch-to-buffer buffername)))
 
-(defun start-shell ()
-  (lambda ()
-    (interactive)
-    (ansi-term "screen" "screen")))
-
 ; views
 (global-set-key (kbd "<f12>") 'org-agenda)
 (global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
 
-;; Jump to a definition in the current file. (This is awesome.)
-(global-set-key (kbd "C-x C-i") 'ido-imenu)
-
 ; org-mode
 (global-set-key (kbd "<f8>") 'org-clock-goto)
 (global-set-key (kbd "C-<f11>") 'org-clock-in)
-(global-set-key (kbd "C-s-<f12>") 'bh/save-then-publish)
 (global-set-key (kbd "M-<f11>") 'org-resolve-clocks)
 (global-set-key (kbd "C-M-r") 'org-capture)
-(global-set-key (kbd "M-<f9>") (lambda ()
-                                 (interactive)
-                                 (unless (buffer-modified-p)
-                                   (kill-buffer (current-buffer)))
-                                 (delete-frame)))
-
-(global-set-key (kbd "<f9> I") 'bh/clock-in)
-(global-set-key (kbd "<f9> O") 'bh/clock-out)
-;(global-set-key (kbd "<f9> r") 'boxquote-region)
 (global-set-key (kbd "<f9> s") 'bh/go-to-scratch)
-;(global-set-key (kbd "<f9> t") 'bh/insert-inactive-timestamp)
-(global-set-key (kbd "<f9> u") 'bh/untabify)
-(global-set-key (kbd "<f9> v") 'visible-mode)
-(global-set-key (kbd "<f9> SPC") 'bh/clock-in-last-task)
+
 
 (defun bh/go-to-scratch ()
   (interactive)
   (switch-to-buffer "*scratch*")
   (delete-other-windows))
 
-(defun bh/untabify ()
-  (interactive)
-  (untabify (point-min) (point-max)))
+; Join from anywhere
+(global-set-key (kbd "M-j")
+                (lambda ()
+                  (interactive)
+                  (join-line -1)))
 
-; fullscreen!
-(defun toggle-fullscreen (&optional f)
-      (interactive)
-      (let ((current-value (frame-parameter nil 'fullscreen)))
-           (set-frame-parameter nil 'fullscreen
-                                (if (equal 'fullboth current-value)
-                                    (if (boundp 'old-fullscreen) old-fullscreen nil)
-                                    (progn (setq old-fullscreen current-value)
-                                           'fullboth)))))
-    (global-set-key [f11] 'toggle-fullscreen)
+;; Move more quickly
+(global-set-key (kbd "C-S-n")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (next-line 5))))
+
+(global-set-key (kbd "C-S-p")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (previous-line 5))))
+
+(global-set-key (kbd "C-S-f")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (forward-char 5))))
+
+(global-set-key (kbd "C-S-b")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (backward-char 5))))
+
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
