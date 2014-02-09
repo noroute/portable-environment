61b3249573780f730bbb3ec65e78451d4d66e41d
Florian Thiel <florian.thiel@epost-dev.de>
2014-02-09 15:51:05 +0100

    [Cleanup] Remove cleanup funcs provided in prelude, get rid of dired-details

--- .emacs.d/conf/07_cleanup.el
+++ .emacs.d/conf/07_cleanup.el
@@ -1,30 +1,9 @@
-; Cleanup functions
-(defun untabify-buffer ()
-  (interactive)
-  (untabify (point-min) (point-max)))
-
-(defun indent-buffer ()
-  (interactive)
-  (indent-region (point-min) (point-max)))
-
-(defun cleanup-buffer ()
-  "Perform a bunch of operations on the whitespace content of a buffer."
-  (interactive)
-  (indent-buffer)
-  (untabify-buffer)
-  (delete-trailing-whitespace))
-
 (defun pretty-lambdas ()
   (font-lock-add-keywords
    nil `(("(?\\(lambda\\>\\)"
           (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                     ,(make-char 'greek-iso8859-7 107))
                     nil))))))
-
-;; Make dired less verbose
-(require 'dired-details)
-(setq-default dired-details-hidden-string "--- ")
-(dired-details-install)
 
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
