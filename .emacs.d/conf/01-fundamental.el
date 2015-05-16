518c145dc9e67656c0a070ab9b71be1575c20935
Florian Thiel <florian.thiel@epost-dev.de>
2015-05-16 19:52:28 +0200

    Make M-v always undo C-v

--- .emacs.d/conf/01-fundamental.el
+++ .emacs.d/conf/01-fundamental.el
@@ -6,6 +6,9 @@
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
+;; M-v always undoes C-v
+(setq scroll-preserve-screen-position 'always)
+
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
