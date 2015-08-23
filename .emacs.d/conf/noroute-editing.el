df42c60c31e67a710e80cacc4841e9e03f49192b
Florian Thiel <florian.thiel@epost-dev.de>
2015-08-23 20:28:36 +0200

    Fix key bindings for mc/

--- .emacs.d/conf/noroute-editing.el
+++ .emacs.d/conf/noroute-editing.el
@@ -2,16 +2,16 @@
 
 ;; Multiple cursors!
 (package-require 'multiple-cursors)
-(global-set-key (kbd "S-<insert>") 'mc/edit-lines)
-(global-set-key (kbd "C-<print>") 'mc/mark-previous-like-this)
-(global-set-key (kbd "<insert>") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-M-<insert>") 'mc/mark-more-like-this-extended)
-(global-set-key (kbd "C-<insert>") 'mc/mark-all-like-this-dwim)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; Visual regexp
 (package-require 'visual-regexp)
 ;(global-set-key (kbd "C-c r") 'vr/replace)
-(global-set-key (kbd "C-c q") 'vr/query-replace)
+;(global-set-key (kbd "C-c q") 'vr/query-replace)
 (global-set-key (kbd "C-c m") 'vr/mc-mark)
 
 (provide 'noroute-editing)
